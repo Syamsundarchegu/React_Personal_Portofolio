c66eb55e2f3ad3aa82c7428db81ca7205bb63250
Syamsundarchegu <chegusyam396@gmail.com>
2024-11-19 15:11:00 +0530

    changes made

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroImage from "../assests/syam.png";
+import HeroImage from "../assests/file.jpg";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 
